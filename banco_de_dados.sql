0a275980e954c4698e9a87a442dae3cc83f5109e
José Henrique <thehique@gmail.com>
2018-07-16 19:52:49 -0300

    upload teste sql

---
+++ banco_de_dados.sql
@@ -0,0 +1,11 @@
+CREATE TABLE uf (
+    id  int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome varchar(255) not null,
+    sigla varchar (2) not null
+);
+
+CREATE TABLE cidade (
+    id int (11) NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    nome varchar (255) NOT NULL,
+    uf_id int (11) NOT NULL
+);
